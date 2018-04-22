c1dc4c4a51d0546e18dae16b15e70cf8a49b228d
Jason Warner <jcwarner@gmail.com>
2018-04-22 18:40:09 -0500

    Add .env file with local server information, added requirement for dotenv npm package

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,9 @@
+// read and set environment variables
+require("dotenv").config();
+
 // Required NPM packages for Bamazon Storefront Application
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const sqlServer = require("./sqlServerSettings");
+
+const connection = mysql.createConnection(sqlServer.connection);
---
+++ sqlServerSettings.js
@@ -0,0 +1,10 @@
+console.log("SQL Server Information Loaded");
+console.log("");
+
+exports.connection = {
+    host: process.env.HOST,
+    port: process.env.PORT,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DB
+};
